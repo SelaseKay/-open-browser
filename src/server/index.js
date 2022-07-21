71872e8eb475f4e9adddeafb252a6c92cceb6bed
SelaseKay <judekwashie70@gmail.com>
2022-07-21 19:26:43 +0000

    completed

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,35 @@
-const {Server} =  require("socket.io");
+const server = require('http').createServer()
+const io =  require("socket.io")(server);
 const open = require('open')
 
-const io = new Server(3000)
 
 io.on("connection", (socket) => {
     console.log("client has connected")
 
     socket.on("link", (args, callback) => {
-        open(args)
+        
+        var pattern = /^((http|https|ftp):\/\/)/;
+        var url = args.link
+
+        if(!pattern.test(url)) {
+            url = "https://" + url
+            console.log(`url; ${url}`)
+            open(url)
+            return
+        }
+
+        console.log(`url outside ifblock; ${url}`)
+        open(url)
     })
+
+    socket.on('disconnect', function () {
+        console.log('client has disconnected');
+      })
 })
+
+var port = process.env.PORT || 3000;
+server.listen(port, function (err) {
+  if (err) console.log(err);
+  console.log('Listening on port', port);
+});
 
